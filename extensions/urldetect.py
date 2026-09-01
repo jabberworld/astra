57b283e36ba571cb29fd7092cf49414b0edb2dd8
EOUpSL93Y <rain@gmx.tm>
2026-09-01 11:34:16 +0300

    urls fix

--- extensions/urldetect.py
+++ extensions/urldetect.py
@@ -7,6 +7,7 @@
 # BETA! 
 import re
 import requests
+from urllib.parse import urlsplit, urlunsplit
 
 
 def urlWatcher(raw, mType, source, body):
@@ -16,30 +17,42 @@ def urlWatcher(raw, mType, source, body):
                                 m = re.search(r'https?://\S+', body)
                                 url = m.group(0).strip(".,;:!?)]}\"") if m else ''
                                 if url:
-                                        if globals().get('chkUnicode') and not chkUnicode(url):
-                                                url = "http://" + IDNA(url)
+                                        if any(ord(c) > 0x7f for c in url):
+                                                try:
+                                                        _p = urlsplit(url)
+                                                        _h = _p.hostname
+                                                        if _h and any(ord(c) > 0x7f for c in _h):
+                                                                _netloc = _h.encode("idna").decode("ascii")
+                                                                if _p.port and _p.port != (443 if _p.scheme == "https" else 80):
+                                                                        _netloc += ":" + str(_p.port)
+                                                                url = urlunsplit((_p.scheme, _netloc, _p.path, _p.query, _p.fragment))
+                                                except Exception:
+                                                        pass
                                         _proxy = globals().get('NETWORK_PROXY')
                                         _timeout = globals().get('NETWORK_TIMEOUT', 20)
                                         proxies = {'http': _proxy, 'https': _proxy} if _proxy else None
-                                        opener = requests.get(url, headers={'User-Agent': UserAgents['Firefox']},
-                                                              proxies=proxies, timeout=_timeout, stream=True)
-                                        ContentType = opener.headers.get("Content-Type") or ""
-                                        if "text/html" in ContentType or url.rstrip("/").endswith((".html", ".htm")):
-                                                data = b''
-                                                for chunk in opener.iter_content(65536):
-                                                        data += chunk
-                                                        if len(data) >= 2048576:
-                                                                break
-                                                data = decode_page(data)
-                                                title = getTagData("title", data)
-                                                answer = u"Заголовок: %s" % uHTML(title).replace("\n", "")
-                                        else:
-                                                Type = ContentType
-                                                Size = byteFormat(int(opener.headers.get("Content-Length") or 0))
-                                                Date = opener.headers.get("Last-Modified") or ""
-                                                answer = u"Тип: %s, размер: %s; последнее изменение файла: %s." % (Type, Size, Date)
-                                        opener.close()
-                                        msg(source[1], answer)
+                                        try:
+                                                opener = requests.get(url, headers={'User-Agent': UserAgents['Firefox']},
+                                                                      proxies=proxies, timeout=_timeout, stream=True)
+                                                ContentType = opener.headers.get("Content-Type") or ""
+                                                if "text/html" in ContentType or url.rstrip("/").endswith((".html", ".htm")):
+                                                        data = b''
+                                                        for chunk in opener.iter_content(65536):
+                                                                data += chunk
+                                                                if len(data) >= 2048576:
+                                                                        break
+                                                        data = decode_page(data)
+                                                        title = getTagData("title", data)
+                                                        answer = u"Заголовок: %s" % uHTML(title).replace("\n", "")
+                                                else:
+                                                        Type = ContentType
+                                                        Size = byteFormat(int(opener.headers.get("Content-Length") or 0))
+                                                        Date = opener.headers.get("Last-Modified") or ""
+                                                        answer = u"Тип: %s, размер: %s; последнее изменение файла: %s." % (Type, Size, Date)
+                                                opener.close()
+                                                msg(source[1], answer)
+                                        except requests.exceptions.RequestException:
+                                                msg(source[1],u'не могу посмотреть инфу о ссылке. :(')
                         except Exception:
                             lytic_crashlog(urlWatcher, command="urldetect")
                             msg(source[1],u'не могу посмотреть инфу о ссылке. :(')
